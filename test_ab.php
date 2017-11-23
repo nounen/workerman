391c2688eb52f0d3c3b0448650cf537708d353c0
nounen <281580047@qq.com>
2017-11-23 17:52:51 +0800

    workerman 换成 linux 版本

--- test_ab.php
+++ test_ab.php
@@ -10,7 +10,7 @@ require_once './vendor/autoload.php';
 $worker = new Worker('tcp://0.0.0.0:1234');
 
 // 进程数配置成cpu核数-1，保留一个cpu给ab进程
-$worker->count=3;
+$worker->count =3;
 
 $worker->onMessage = function ($connection, $data) {
     $connection->send("HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nServer: workerman\1.1.4\r\n\r\nhello");
